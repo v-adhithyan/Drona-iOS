a855ece6293d4930c0ab14507ce08f53e842894d
v-adhithyan <aavispeaks@gmail.com>
2018-07-24 09:11:29 +0530

    initial commit

--- Drona/ViewController.swift
+++ Drona/ViewController.swift
@@ -7,19 +7,42 @@
 //
 
 import UIKit
+import FeedKit
+import MBProgressHUD
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        parseFeed()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    func parseFeed() {
+        let loadingNotification = MBProgressHUD.showAdded(to: view, animated: true)
+        loadingNotification.mode = MBProgressHUDMode.indeterminate
+        loadingNotification.label.text = "Loading"
+        let feedURL = URL(string: "https://jrvarma.wordpress.com/feed")!
+        let parser = FeedParser(URL: feedURL)
+        parser.parseAsync(queue: DispatchQueue.global(qos: .userInitiated)) { (result) in
+            DispatchQueue.main.async {
+                guard let feed = result.rssFeed, result.isSuccess else {
+                    print(result.error ?? "")
+                    return
+                }
+                let item = feed.items?.first
+                print(item?.title)
+                print(item?.link)
+                print(item?.description)
+                MBProgressHUD.hideAllHUDs(for: self.view, animated: true)
+            }
+            
+        }
+    }
+    
 
 }
 
---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Drona' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Drona
+   pod 'FeedKit', '~> 8.0'
+   pod 'MBProgressHUD', '~> 1.1.0'
+
+  target 'DronaTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'DronaUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
